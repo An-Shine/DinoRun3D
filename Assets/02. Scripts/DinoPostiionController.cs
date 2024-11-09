72f0112eec7e00ffb7f8c81b56dc40ab9d5c4bd9
An-Shine <akh1012@naver.com>
2024-11-09 13:36:19 +0900

    asdf

--- Assets/02. Scripts/DinoPostiionController.cs
+++ Assets/02. Scripts/DinoPostiionController.cs
@@ -8,7 +8,7 @@ public class DinoPostiionController : MonoBehaviour
     public float ratio = 0.1f; // 배치간격비율( 작을수록 촘촘)
     public Transform raptors;  // raptor 를 관리할 부모 오브젝트
     public int visibleCount; // 보여주고싶은 갯수를 입력하면 이 갯수만큼만 dino 표시
-
+    float angleStep;
 
 
     void Start()
@@ -34,16 +34,38 @@ public class DinoPostiionController : MonoBehaviour
 
     private void SetDinoPosition()
     {
-        float angleStep = 360f / (raptors.childCount*ratio); // 360도를 Dino 갯수로 나눈값, 각도의 증가값
+        
         for (int i = 0; i<raptors.childCount; i++)
         {
-            float angle = i * angleStep;  //각 개인자식 오브젝트의 배치각도계산
-            float angleRad = Mathf.Deg2Rad * angle;  // x,z 축 좌표를 구해야함
-            float x = Mathf.Cos(angleRad) * radius;
-            float z = Mathf.Sin(angleRad) * radius;
+            if (i >= visibleCount)
+            {
+                raptors.GetChild(i).gameObject.SetActive(false);
+                continue;
+            }
+            else
+            {
+                if(raptors.childCount >= visibleCount)
+                {
+                    angleStep = 360f / (visibleCount * ratio);
+                }
+                else
+                {
+                    angleStep = 360f / (raptors.childCount * ratio);
+                }
+                
+                float angle = i * angleStep;  //각 개인자식 오브젝트의 배치각도계산
+                float angleRad = Mathf.Deg2Rad * angle;  // x,z 축 좌표를 구해야함
+                float x = Mathf.Cos(angleRad) * radius;
+                float z = Mathf.Sin(angleRad) * radius;
 
-            raptors.GetChild(i).localPosition = new Vector3(x, 0, z);
+                raptors.GetChild(i).localPosition = new Vector3(x, 0, z);
+                
+            }
+           
         }
-    }
 
+     
+        
+    }
+    
 }
---
+++ Assets/02. Scripts/MapManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public GameObject[] mapPrefabs;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
