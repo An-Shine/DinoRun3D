df30f0cda73d427ced4376494caed5696bad7aea
An-Shine <akh1012@naver.com>
2024-11-30 12:00:53 +0900

    ddd

--- Assets/02. Scripts/GameManager.cs
+++ Assets/02. Scripts/GameManager.cs
@@ -7,8 +7,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public bool isGameStart;
-    
+
     public GameObject TitlePanel;
+    public GameObject gamePanel;
     public Slider progressBar;
     public GameObject goalObject;
     public GameObject Dino;
@@ -34,24 +35,22 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        SetDistanceProgressBar();
     }
 
     public void GameStart()
     {
         Time.timeScale = 1f;
         isGameStart = true;
-        if (isGameStart.Equals(true))
-        {
-            TitlePanel.SetActive(false);
-        }
+        TitlePanel.SetActive(false);
+        gamePanel.SetActive(true);
     }
     public void SetDistanceProgressBar() //프로그래스 바 세팅함수
     {
         if(isGameStart.Equals(true))
         {
             //전체거리중 Dino 의 위치 거리 비율
-            float goalDistance = Dino.transform.position.z / goalObject.transform.position.z;
+            float goalDistance = DinoController.instance.transform.position.z/MapManager.instance.GetGoalDistance();
             progressBar.value = goalDistance;
         }
     }
--- Assets/02. Scripts/MapManager.cs
+++ Assets/02. Scripts/MapManager.cs
@@ -7,6 +7,7 @@ public class MapManager : MonoBehaviour
     public GameObject[] mapPrefabs;
     public GameObject goalObject;
     public static MapManager instance;
+    public GameObject[] testMapPrefabs;  
     public void Awake()
     {
         if (instance != null)
@@ -24,7 +25,8 @@ public class MapManager : MonoBehaviour
 
     void Start()
     {
-        CreateMap();
+        // CreateMap();
+        CreateTestMap();
         goalObject = GameObject.FindWithTag("Goal");  //Goal 오브젝트를 찾아서 대입
     }
     private void CreateMap()
@@ -46,8 +48,27 @@ public class MapManager : MonoBehaviour
                 mapPosition.z += nowMap.GetComponent<Map>().GetMapSize() / 2; // 현재 선택된 맵의 길이의 절반 더하기
         }
     }
+    private void CreateTestMap()
+    {
+        //초기화
+        Vector3 mapPosition = Vector3.zero; // 기준 000으로 초기화
+        for (int i = 0; i < testMapPrefabs.Length; i++)
+        {
+            GameObject selectedMap = testMapPrefabs[i];
+            if (i > 0) // 두번째 맵부터 선택된 맵 크기의 반을 더해준다.
+            {
+                mapPosition.z += selectedMap.GetComponent<Map>().GetMapSize() / 2;
+            }
+            else
+            {
+                selectedMap = mapPrefabs[0];   // 첫번째는 무조건 0번째배열의 인덱스의 map 이 생성된다.
+            }
+            GameObject nowMap = Instantiate(selectedMap, mapPosition, Quaternion.identity);
+            mapPosition.z += nowMap.GetComponent<Map>().GetMapSize() / 2; // 현재 선택된 맵의 길이의 절반 더하기
+        }
+    }
 
-    
+
     void Update()
     {
         
